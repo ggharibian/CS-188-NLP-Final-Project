c0cfa85ba55760b595ad76bddc74c96ff37db26b
ggharibian <gharibian.garni0@gmail.com>
2022-09-28 10:06:34 -0700

    Initial commit
    Initial commit with all of the components needed for getting started
    with this project.

---
+++ trainers/train_utils.py
@@ -0,0 +1,51 @@
+import csv
+import glob
+import json
+import random
+import logging
+import os
+from enum import Enum
+from typing import List, Optional, Union
+
+import tqdm
+import numpy as np
+
+import torch
+from transformers import (
+    WEIGHTS_NAME,
+    AdamW,
+    AutoConfig,
+    AutoModelForMaskedLM,
+    AutoTokenizer,
+)
+
+
+def pairwise_accuracy(guids, preds, labels):
+
+    acc = 0.0  # The accuracy to return.
+    
+    ########################################################
+    # TODO: Please finish the pairwise accuracy computation.
+    # Hint: Utilize the `guid` as the `guid` for each
+    # statement coming from the same complementary
+    # pair is identical. You can simply pair the these
+    # predictions and labels w.r.t the `guid`. 
+    raise NotImplementedError("Please finish the TODO!")
+    # End of TODO
+    ########################################################
+     
+    return acc
+
+
+if __name__ == "__main__":
+
+    # Unit-testing the pairwise accuracy function.
+    guids = [0, 0, 1, 1, 2, 2, 3, 3]
+    preds = np.asarray([0, 0, 1, 0, 0, 1, 1, 1])
+    labels = np.asarray([1, 0,1, 0, 0, 1, 1, 1])
+    acc = pairwise_accuracy(guids, preds, labels)
+    
+    if acc == 0.75:
+        print("Your `pairwise_accuracy` function is correct!")
+    else:
+        raise NotImplementedError("Your `pairwise_accuracy` function is INCORRECT!")
